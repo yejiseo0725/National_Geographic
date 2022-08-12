fb90fc65cd5fed7b0a0e2738ec6ab3a96717a402
yeji seo <yejiseo0725@gmail.com>
2022-08-12 17:59:55 +0900

    레이아웃 완성

--- js/NG.js
+++ js/NG.js
@@ -14,21 +14,22 @@ $(function () {
   const videoOffset = $(".sc2 .video-area").offset().top;
   //   console.log(videoOffset);
   const s3offset = $(".sc3").offset().top;
+  const s5offset = $(".sc5").offset().top;
+  const s6offset = $(".sc6").offset().top;
+  console.log(s5offset);
 
   $(window).scroll(function () {
     const now = $(this).scrollTop();
     const nowScrollTop = parseInt(now);
+    const $winH = $(window).height();
 
-    // console.log(nowScrollTop);
+    console.log(nowScrollTop);
 
     if (nowScrollTop >= videoOffset && nowScrollTop <= s3offset) {
       $(".sc2 .video-area .bg-video").css({
         position: "fixed",
       });
       $(".sc2 .text-area").css({
-        position: "fixed",
-      });
-      $(".sc2 .rolling-banner").css({
         position: "fixed",
       });
       $(".sc2 .scr-down-line").css({
@@ -41,17 +42,34 @@ $(function () {
       $(".sc2 .text-area").css({
         position: "",
       });
-      $(".sc2 .rolling-banner").css({
-        position: "",
-      });
       $(".sc2 .scr-down-line").css({
         position: "",
       });
     }
 
-    // if (nowScrollTop >= s3offset) {
-    //   $(".sc3").css({
+    if (nowScrollTop >= s5offset) {
+      $(".sc5 .sc5-inner").css({
+        position: "fixed",
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%, -50%)",
+      });
+    } else {
+      $(".sc5 .sc5-inner").css({
+        position: "",
+        left: "",
+        top: "",
+        transform: "",
+      });
+    }
+
+    // if (nowScrollTop + $winH * 0.5 >= s6offset) {
+    //   $(".sc6").css({
     //     position: "fixed",
+    //   });
+    // } else {
+    //   $(".sc6").css({
+    //     position: "",
     //   });
     // }
   });
