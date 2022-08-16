22fbf1786f39220d49b323c3496e2c73fd555992
yeji seo <yejiseo0725@gmail.com>
2022-08-16 17:54:16 +0900

    update

--- js/NG.js
+++ js/NG.js
@@ -15,10 +15,10 @@ $(function () {
   const mainoffset = $(".visual-section .title-area").offset().top;
 
   const s1offset = $(".sc1").offset().top;
-  const s1Titleoffset = $(".sc1 .title-area h3:first-child").offset().top;
-  console.log(s1offset, s1Titleoffset);
+  const s1h3offset = $(".sc1 .title-area h3:first-child").offset().top;
   const s3offset = $(".sc3").offset().top;
   const s4offset = $(".sc4").offset().top;
+  const s4poffset = $(".sc4 .article-area article p").offset().top;
   const s5offset = $(".sc5").offset().top;
 
   const $winH = $(window).height();
@@ -39,8 +39,26 @@ $(function () {
       });
     }
 
-    if (nowScrollTop >= s1Titleoffset) {
-      $(".sc1 .article-area article:nth-child(1)").css({});
+    if (nowScrollTop >= s1h3offset) {
+      $(".sc1 .article-area article:nth-child(1)").css({
+        transform: "translateY(" + -nowScrollTop * 0.1 + "px)",
+      });
+      $(".sc1 .article-area article:nth-child(2)").css({
+        transform: "translateY(" + -nowScrollTop * 0.15 + "px)",
+      });
+      $(".sc1 .article-area article:nth-child(3)").css({
+        transform: "translateY(" + -nowScrollTop * 0.08 + "px)",
+      });
+    } else {
+      $(".sc1 .article-area article:nth-child(1)").css({
+        transform: "",
+      });
+      $(".sc1 .article-area article:nth-child(2)").css({
+        transform: "",
+      });
+      $(".sc1 .article-area article:nth-child(3)").css({
+        transform: "",
+      });
     }
 
     if (nowScrollTop >= videoOffset && nowScrollTop <= s3offset) {
@@ -71,20 +89,10 @@ $(function () {
       $(".box2 .img-area .society-img").removeClass("active");
     }
 
-    if (nowScrollTop >= s5offset) {
-      $(".sc5 .sc5-inner").css({
-        position: "fixed",
-        left: "50%",
-        top: "50%",
-        transform: "translate(-50%, -50%)",
-      });
+    if (nowScrollTop >= s4poffset) {
+      $(".sc5 .text-area").addClass("active");
     } else {
-      $(".sc5 .sc5-inner").css({
-        position: "",
-        left: "",
-        top: "",
-        transform: "",
-      });
+      $(".sc5 .text-area").removeClass("active");
     }
   });
 });
